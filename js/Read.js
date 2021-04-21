67765d01c9ad432212fb91c8dd0d16bde77f3e85
Raiymbek Kumarbekov <raimbek965@gmail.com>
2021-04-21 16:55:55 +0600

    first commit

--- js/Read.js
+++ js/Read.js
@@ -1,10 +1,20 @@
 
 console.log(firebase);
 
-var ref = firebase.database().ref("AudioFiles/-MJfrapJBKLwO2qIfU3y/audioUrl");
+// var ref = firebase.database().ref("AudioFiles/-MJfrapJBKLwO2qIfU3y/audioUrl");
 
-ref.on("value", function(snapshot) {
-   console.log(snapshot.val());
-}, function (error) {
-   console.log("Error: " + error.code);
-});
+// ref.on("value", function(snapshot) {
+//    console.log(snapshot.val());
+// }, function (error) {
+//    console.log("Error: " + error.code);
+// });
+
+
+const db = firebase.firestore();
+
+db.collection("meditations")
+.doc("rXDLCItJsa0EWZXWak0O")
+.get()
+.then(doc => {
+    console.log(doc.data())
+})
